ea7027e27ac000729865108d8ef7751443ff3078
rusgall <rusgall@rus.uni-stuttgart.de>
2017-09-25 15:24:51 +0200

    Create rerootable class

---
+++ src/com/github/gallardo/Rerootable.java
@@ -0,0 +1,9 @@
+package com.github.gallardo;
+
+/** In main */
+public class Rerootable {
+    /** In main */
+    public static final String getSample() {
+        return "Sample string from main";
+    }
+}
---
+++ src/com/github/gallardo/super_src/com/github/gallardo/Rerootable.java
@@ -0,0 +1,9 @@
+package com.github.gallardo;
+
+/** In super_src */
+public class Rerootable {
+    /** In super_src */
+    public static final String getSample() {
+        return "Sample string from super_src";
+    }
+}
